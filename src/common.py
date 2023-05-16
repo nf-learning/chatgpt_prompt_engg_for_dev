317aaa469c56f8d5df6b60889f14e5e748543935
Nigel Faria <nigel.faria@gmail.com>
2023-05-16 21:57:35 +0100

    Expanding - Create automated replies. Sign off as AI - Temperature - Degree of randomness. More is more.

--- src/common.py
+++ src/common.py
@@ -6,11 +6,11 @@ _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     messages = [{"role":"user","content":prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0,
+        temperature=temperature,
     )
     return response.choices[0].message["content"]
