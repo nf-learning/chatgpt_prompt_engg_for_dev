4f935252b14201bdb8ac42f02ae70197ab048d3c
Nigel Faria <nigel.faria@gmail.com>
2023-05-25 14:53:11 +0100

    Creating a Chat bot - Conversations are standalone and prior conversations have to be passed in as context - Roles - System, User, Assistant - Context - Who, What steps, How, What output, What information to know

--- src/common.py
+++ src/common.py
@@ -14,3 +14,13 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
         temperature=temperature,
     )
     return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+    )
+    return response.choices[0].message["content"]
+
+
